0b8e5c87bfc64f4bd40046852c83cee2e897cb3f
Gleb Mickevich <glebmickevich@gmail.com>
2022-03-05 05:39:49 +0200

    Lesson 2: Classes

---
+++ Task_3/BankAccount_3.cs
@@ -0,0 +1,63 @@
+﻿// 3. В классе банковский счет удалить методы заполнения полей.
+// Вместо этих методов создать конструкторы.
+// Переопределить конструктор по умолчанию, создать конструктор для заполнения поля баланс,
+// конструктор для заполнения поля тип банковского счета, конструктор для заполнения баланса
+// и типа банковского счета. Каждый конструктор должен вызывать метод, генерирующий номер счета.
+
+
+var privateBankAccount = new BankAccount(150.00);
+var publicBankAccount = new BankAccount(BankAccountType.Public);
+var anotherPrivateBankAccount = new BankAccount(130.00, BankAccountType.Private);
+
+Console.WriteLine($"Account Number: {privateBankAccount.GetAccountId()}" +
+    $"\nAccount Type: {privateBankAccount.GetAccountType()}\nAccount Balance: {privateBankAccount.GetBalance()}");
+
+Console.WriteLine($"Account Number: {publicBankAccount.GetAccountId()}" +
+    $"\nAccount Type: {publicBankAccount.GetAccountType()}\nAccount Balance: {publicBankAccount.GetBalance()}");
+
+Console.WriteLine($"Account Number: {anotherPrivateBankAccount.GetAccountId()}" +
+    $"\nAccount Type: {anotherPrivateBankAccount.GetAccountType()}\nAccount Balance: {anotherPrivateBankAccount.GetBalance()}");
+
+public enum BankAccountType
+{
+    Private,
+    Public
+}
+
+public class BankAccount
+{
+    public int _accountId;
+    public static int _accountNumber = 0;
+    private double _balance;
+    private BankAccountType _accountType;
+
+    public BankAccount(double balance)
+    {
+        _balance = balance;
+        IncreaseAccountNumber();
+    }
+
+    public BankAccount(BankAccountType bankAccountType)
+    {
+        _accountType = bankAccountType;
+        IncreaseAccountNumber();
+    }
+
+    public BankAccount(double balance, BankAccountType bankAccountType)
+    {
+        _balance = balance;
+        _accountType = bankAccountType;
+        IncreaseAccountNumber();
+    }
+
+
+    public int GetAccountId() { return _accountId; }    
+    public double GetBalance() { return _balance; }
+    public BankAccountType GetAccountType() { return _accountType; }
+
+    public void IncreaseAccountNumber() { 
+        _accountNumber++;
+        _accountId = _accountNumber;
+    }
+
+}
---
+++ gb_oop_basics/BankAccount_1.cs
@@ -0,0 +1,32 @@
+﻿// 1. Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип).
+// Предусмотреть методы для доступа к данным – заполнения и чтения.
+// Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.
+
+var privateBankAccount = new BankAccount();
+privateBankAccount.SetAccountNumber(1);
+privateBankAccount.SetBalance(100.00);
+privateBankAccount.SetBankAccountType(BankAccountType.Private);
+
+Console.WriteLine($"Account Number: {privateBankAccount.GetAccountNumber()}" +
+    $"\nAccount Type: {privateBankAccount.GetAccountType()}\nAccount Balance: {privateBankAccount.GetBalance()}");
+
+public enum BankAccountType
+{
+    Private,
+    Public
+}
+
+public class BankAccount
+{
+    private int _accountNumber;
+    private double _balance;
+    private BankAccountType _accountName;
+
+    public int GetAccountNumber() { return _accountNumber; }
+    public double GetBalance() { return _balance; }
+    public BankAccountType GetAccountType() { return _accountName;}
+
+    public void SetAccountNumber(int accountNumber) { _accountNumber = accountNumber; }
+    public void SetBalance(double balance) { _balance = balance; }
+    public void SetBankAccountType(BankAccountType bankAccountType) { _accountName = bankAccountType; }
+}
